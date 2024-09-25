66f815f60a70c396f3fcf489b19acc38f2babbe2
Rhedaligan <Rhedaligan8@gmail.com>
2024-09-25 20:42:00 +0800

    #39uppercasegreeting

---
+++ src/app/components/uppercasegreeting/uppercasegreeting.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uppercasegreeting',
+  templateUrl: './uppercasegreeting.component.html',
+  styleUrl: './uppercasegreeting.component.css'
+})
+export class UppercasegreetingComponent {
+  name: string = '';
+  uppercaseName: string | null = null;
+
+  displayUppercaseName(): void {
+    this.uppercaseName = this.name.toUpperCase();
+  }
+}
