0c26822538f1456323357bffb5072bd8a47974e1
Rhedaligan <Rhedaligan8@gmail.com>
2024-09-25 18:55:01 +0800

    #27randomnumbergenerator

---
+++ src/app/components/randomnumbergenerator/randomnumbergenerator.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-randomnumbergenerator',
+  templateUrl: './randomnumbergenerator.component.html',
+  styleUrl: './randomnumbergenerator.component.css'
+})
+export class RandomnumbergeneratorComponent {
+  min: number = 1;
+  max: number = 100;
+  randomNumber: number | null = null;
+
+  generateRandomNumber(): void {
+    this.randomNumber = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+  }
+}
